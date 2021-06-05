ae670ea0d23e8a43ea067519a87d255dfa52e92b
elmos@MBP-de-elmos <elmos@MBP-de-elmos>
2021-06-05 18:45:56 +0200

    docker at Sam  5 jui 2021 18:45:56 CEST

---
+++ docker/lance.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/eldagithub/dockerjenkins.git
+dockerjenkins/
+
+gitcom
+git push
+
+docker build -t myjenkins-blueocean:1.1 .
+
+docker run --name jenkins-blueocean --rm --detach   --network jenkins --env DOCKER_HOST=tcp://docker:2376   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1   --publish 8080:8080 --publish 50000:50000   --volume jenkins-data:/var/jenkins_home   --volume jenkins-docker-certs:/certs/client:ro   myjenkins-blueocean:1.1
+
+docker logs 89b8349319dec293f76aa7c1f1e7fad406d37512f2fca62f5da7bfaf3f868d96
